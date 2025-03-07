86ea14850e7e47e9ff38b443f8352c282a967bb3
CRaph0705 <raphaelcochrane@hotmail.fr>
2025-03-07 01:04:31 +0100

    on server side got pid, on client side preparing message

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 14:51:40 by rcochran          #+#    #+#             */
-/*   Updated: 2025/03/06 20:25:26 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/03/07 01:02:57 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,20 @@
 
 // client must convert msg in binary and send it to server
 
-// step 1 convert in binary
-// step 2 send it
+// step 1 need server PID
+// step 2 need the str to send
+// convert in binary then send
 int	main(int ac, char **av)
 {
+	int		server_pid;
+	char	*text;
+
+	if (ac != 3)
+		return (0);
+	server_pid = ft_atoi(av[1]);
+	if (server_pid == 0)
+		return (0);
+	text = av[2];
+	(void)av;
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 14:51:42 by rcochran          #+#    #+#             */
-/*   Updated: 2025/03/06 20:21:16 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/03/07 00:29:17 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,7 @@ int	main(int ac, char **av)
 
 	pid = getpid();
 	ft_printf("server pid : %i", pid);
+	(void)ac;
+	(void)av;
 	return (0);
 }
