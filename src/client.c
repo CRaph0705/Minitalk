50b190c250ccca3adb431bbe0f824f62169df188
CRaph0705 <raphaelcochrane@hotmail.fr>
2025-03-07 16:55:39 +0100

    conversion ok, sigusr1 or 2 logic added

--- src/client.c
+++ src/client.c
@@ -6,7 +6,7 @@
 /*   By: rcochran <rcochran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 14:51:40 by rcochran          #+#    #+#             */
-/*   Updated: 2025/03/07 16:04:15 by rcochran         ###   ########.fr       */
+/*   Updated: 2025/03/07 16:54:31 by rcochran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +18,46 @@
 // step 2 need the str to send
 // convert in binary then send
 
-int	send_sig(int pid, char *to_send);
+void	send_sig(int pid, unsigned char *to_send);
 
 /* 
 Problematic :
 You can only use these two signals: SIGUSR1 and SIGUSR2. 
 => sigusr1 == true // sigusr2 == true -> send 1 or 0
+=> usleep() pour ajouter un délai entre chaque envoi ?
 */
-int	send_sig(int pid, char *to_send)
+void	send_sig(int pid, unsigned char *to_send)
 {
-	// int			sig;
-	// char		*binary;
+	char	*sig;
+	int		i;
+	int		bit;
 
-	// sig = 0;
-	// binary = ft_convert_base(to_send, "0123456789", "01");
-	// ft_printf("pid : %d\n", pid);
-	// ft_printf("binary conversion of to_send (%s) is : %s\n", to_send, binary);
-	ft_printf("65 en binaire: %s\n", ft_convert_base("65", "0123456789", "01"));
-	(void)to_send;
-	(void)pid;
-	return (0);
+	i = 0;
+	while (to_send[i])
+	{
+		sig = ft_convert_base(to_send[i], "0123456789", "01");
+		if (!sig)
+			return ;
+		ft_printf("binary conversion (%c) is : %s\n", to_send[i], sig);
+		bit = 0;
+		while (sig[bit])
+		{
+			if (sig[bit] == 0)
+				kill(pid, SIGUSR1);
+			else if (sig[bit] == 1)
+				kill(pid, SIGUSR2);
+			bit++;
+		}
+		free(sig);
+		i++;
+	}
+	ft_printf("pid : %d\n", pid);
 }
 
 int	main(int ac, char **av)
 {
-	int		server_pid;
-	char	*to_send;
-	int		i;
+	int				server_pid;
+	unsigned char	*to_send;
 
 	if (ac != 3)
 		return (0);
@@ -52,10 +65,6 @@ int	main(int ac, char **av)
 	if (server_pid == 0)
 		return (0);
 	to_send = av[2];
-	i = 0;
 	send_sig(server_pid, to_send);
-	(void)i;
-	// while (to_send[i++])
-	// 	send_sig(server_pid, to_send[i]);
 	return (0);
 }
